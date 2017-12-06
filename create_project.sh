e8812557fee958e0a4431182cfec5bd09a4b9c95
Simon Kassel <kassel.simon@gmail.com>
2017-12-06 10:25:11 -0500

    update bash script

--- create_project.sh
+++ create_project.sh
@@ -2,15 +2,15 @@
 
 cd ..
 
-rm -rf /R-Project-Template
-rm -rf $1
+# rm -rf /R-Project-Template
+rm -rf $1rm 
+mkdir $1
 
-git clone https://github.com/simonkassel/R-Project-Template
+# git clone https://github.com/simonkassel/R-Project-Template
 rm -rf R-Project-Template/.git R-Project-Template/README.md R-Project-Template/create_project.sh
 
-mkdir $1
 cd $1
 
 mv ../R-Project-Template/* .
 touch README.md
-rm -r -f ../R-Project-Template
+rm -rf ../R-Project-Template
