c8758b1eb98e03a0eb360de9298198d21e8f5651
Simon Kassel <kassel.simon@gmail.com>
2017-12-06 09:57:58 -0500

    add shell script to create project

---
+++ create_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf /R-Project-Template
+rm -rf $1
+
+git clone https://github.com/simonkassel/R-Project-Template
+rm -rf R-Project-Template/.git R-Project-Template/README.md R-Project-Template/create_project.sh
+
+mkdir $1
+cd $1
+
+mv ../R-Project-Template/* .
+touch README.md
+rm -r -f ../R-Project-Template
